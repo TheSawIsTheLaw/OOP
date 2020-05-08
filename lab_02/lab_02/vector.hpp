e3c222aecda21573037feb03eafc7368a7aad5d2
xAMATEURx <dimakrok@bk.ru>
2020-05-09 00:25:10 +0300

    FIX: vectorSize change

--- lab_02/lab_02/vector.hpp
+++ lab_02/lab_02/vector.hpp
@@ -124,6 +124,8 @@ Vector<Type>::Vector(int len) {
         throw MemoryException(__FILE__, typeid(*this).name(),
                               __LINE__, ctime(&currentTime));
 
+    vectorSize = len;
+
     Iterator<Type> iterator(*this);
     for (; iterator; iterator++)
         *iterator = 0;
