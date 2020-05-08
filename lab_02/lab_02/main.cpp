6f8d27376231081624e0901bf4fae3295965aa84
xAMATEURx <dimakrok@bk.ru>
2020-05-09 00:31:24 +0300

    ADD: test for empty vector

--- lab_02/lab_02/main.cpp
+++ lab_02/lab_02/main.cpp
@@ -6,8 +6,10 @@ int main(void)
     Vector<int> initializerListExample = {1, 2, 3, 4, 5};
     Vector<float> lenExample(3);
     Vector<double> lenFillExample(4, 6.66);
+    Vector<long double> emptyVector(0);
 
     std::cout<< "initializerListInit: "<< initializerListExample<< std::endl;
     std::cout<< "lenInit: "<< lenExample<< std::endl;
     std::cout<< "LenFillInit with 6.66 filler: "<< lenFillExample<< std::endl;
+    std::cout<< "Empty vector: "<< emptyVector<< std::endl;
 }
