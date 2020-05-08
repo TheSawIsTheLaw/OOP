3513596e836e68ec1628a0836b5da5e53d0fd1fe
xAMATEURx <dimakrok@bk.ru>
2020-05-09 00:24:50 +0300

    ADD: new check

--- lab_02/lab_02/main.cpp
+++ lab_02/lab_02/main.cpp
@@ -6,4 +6,7 @@ int main(void)
     Vector<int> initializerListExample = {1, 2, 3, 4, 5};
     Vector<float> lenExample(3);
     Vector<double> lenFillExample(4, 6.66);
+
+    std::cout<< "initializerListInit: "<< initializerListExample<< std::endl;
+    std::cout<< "lenInit: "<< lenExample<< std::endl;
 }
