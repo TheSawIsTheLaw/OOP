0fa7acd3fb2cf18b29dc6f724cd0be62c3ba9741
xAMATEURx <dimakrok@bk.ru>
2020-05-08 21:37:57 +0300

    ADD: first declarations

--- lab_02/lab_02/main.cpp
+++ lab_02/lab_02/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "vector.h"
 
-int main()
+int main(void)
 {
-
+    Vector<int> initializerListExample = {1, 2, 3, 4, 5};
+    Vector<float> lenExample(3);
+    Vector<double> lenFillExample(4, 6.66);
 }
