5952fd56d796d952378dcc9dfc55982b03d47da4
xAMATEURx <dimakrok@bk.ru>
2020-05-11 19:44:33 +0300

    ADD: new test

--- lab_02/lab_02/main.cpp
+++ lab_02/lab_02/main.cpp
@@ -8,11 +8,16 @@ int main(void)
     Vector<double> lenFillExample(3, 6.66);
     Vector<long double> emptyVector(0);
 
+    Vector<int> operVecF = {1, 2, 3};
+    Vector<int> operVecS = {3, 2, 4};
+
     std::cout<< "initializerListInit: "<< initializerListExample<< std::endl;
     std::cout<< "lenInit: "<< lenExample<< std::endl;
     std::cout<< "LenFillInit with 6.66 filler: "<< lenFillExample<< std::endl;
     std::cout<< "Empty vector: "<< emptyVector<< std::endl;
 
     std::cout<< "LenFill and InitList angle: "<< lenFillExample.vectorsAngle(initializerListExample)<< std::endl;
+    std::cout<< "Vectors *: "<< operVecF * operVecS<< std::endl;
+    std::cout<< "Vectors +: "<< operVecF + operVecS<< std::endl;
 
 }
