ccfc1a2666fe20dc6ce7f9f0705337582671dfc4
xAMATEURx <dimakrok@bk.ru>
2020-05-11 19:33:32 +0300

    ADD: new test for vectorsAngle

--- lab_02/lab_02/main.cpp
+++ lab_02/lab_02/main.cpp
@@ -3,14 +3,16 @@
 
 int main(void)
 {
-    Vector<double> initializerListExample = {1, 2, 3, 4, 5};
+    Vector<double> initializerListExample = {1, 2, 3};
     Vector<float> lenExample(3);
-    Vector<double> lenFillExample(4, 6.66);
+    Vector<double> lenFillExample(3, 6.66);
     Vector<long double> emptyVector(0);
 
     std::cout<< "initializerListInit: "<< initializerListExample<< std::endl;
     std::cout<< "lenInit: "<< lenExample<< std::endl;
     std::cout<< "LenFillInit with 6.66 filler: "<< lenFillExample<< std::endl;
     std::cout<< "Empty vector: "<< emptyVector<< std::endl;
+
+    std::cout<< "LenFill and InitList angle: "<< lenFillExample.vectorsAngle(initializerListExample)<< std::endl;
 
 }
