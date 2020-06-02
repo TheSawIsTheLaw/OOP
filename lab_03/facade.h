aec15a7d2077ce0f15c354f19d72c744a47f4155
xAMATEURx <dimakrok@bk.ru>
2020-06-03 00:03:39 +0300

    ADD: upload to facade

--- lab_03/facade.h
+++ lab_03/facade.h
@@ -1,11 +1,13 @@
 #ifndef FACADE_H
 #define FACADE_H
 
+#include "Managers/UploadManager.h"
+
 class Facade{
 public:
 //    CommandBase Command();
 private:
-//    UploadManager Upload;
+    UploadManager Upload;
 //    SceneManager Scene;
 //    DrawManager Draw;
 //    TransformManager Transform;
