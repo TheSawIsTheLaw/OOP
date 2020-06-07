4ac906dd5afcd5fb053818676b286466bf905fff
xAMATEURx <dimakrok@bk.ru>
2020-06-07 18:13:57 +0300

    ADD: slots

--- lab_04/mainwindow.cpp
+++ lab_04/mainwindow.cpp
@@ -10,3 +10,67 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow() {
     delete ui;
 }
+
+void MainWindow::on_CabinButtonFloor1_clicked() {}
+
+void MainWindow::on_CabinButtonFloor2_clicked() {}
+
+void MainWindow::on_CabinButtonFloor3_clicked() {}
+
+void MainWindow::on_CabinButtonFloor4_clicked() {}
+
+void MainWindow::on_CabinButtonFloor5_clicked() {}
+
+void MainWindow::on_CabinButtonFloor6_clicked() {}
+
+void MainWindow::on_CabinButtonFloor7_clicked() {}
+
+void MainWindow::on_CabinButtonFloor8_clicked() {}
+
+void MainWindow::on_CabinButtonFloor9_clicked() {}
+
+void MainWindow::on_CabinButtonFloor10_clicked() {}
+
+void MainWindow::on_CabinButtonFloor11_clicked() {}
+
+void MainWindow::on_CabinButtonFloor12_clicked() {}
+
+void MainWindow::on_CabinButtonFloor13_clicked() {}
+
+void MainWindow::on_CabinButtonFloor14_clicked() {}
+
+void MainWindow::on_CabinButtonFloor15_clicked() {}
+
+void MainWindow::on_CabinButtonFloor16_clicked() {}
+
+void MainWindow::on_FloorButton1_clicked() {}
+
+void MainWindow::on_FloorButton2_clicked() {}
+
+void MainWindow::on_FloorButton3_clicked() {}
+
+void MainWindow::on_FloorButton4_clicked() {}
+
+void MainWindow::on_FloorButton5_clicked() {}
+
+void MainWindow::on_FloorButton6_clicked() {}
+
+void MainWindow::on_FloorButton7_clicked() {}
+
+void MainWindow::on_FloorButton8_clicked() {}
+
+void MainWindow::on_FloorButton9_clicked() {}
+
+void MainWindow::on_FloorButton10_clicked() {}
+
+void MainWindow::on_FloorButton11_clicked() {}
+
+void MainWindow::on_FloorButton12_clicked() {}
+
+void MainWindow::on_FloorButton13_clicked() {}
+
+void MainWindow::on_FloorButton14_clicked() {}
+
+void MainWindow::on_FloorButton15_clicked() {}
+
+void MainWindow::on_FloorButton16_clicked() {}
