3212f5d4765add50efe1ba5c2277060b710ae1af
xAMATEURx <dimakrok@bk.ru>
2020-06-12 20:44:17 +0300

    ADD: new components

--- lab_03/Composite/component.h
+++ lab_03/Composite/component.h
@@ -1,6 +1,10 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include "../Camera/camerabase.h"
+#include "../Model/model.h"
+#include "../Vector/Vector.h"
+
 class Component
 {
 public:
@@ -12,7 +16,41 @@ public:
     virtual void del() = 0;
     virtual bool isComposite() = 0;
 
-    virtual bool isVisible() = 0;
+    virtual bool isVisible() const noexcept = 0;
+};
+
+class ModelComponent : public Component
+{
+private:
+    shared_ptr<Model> model;
+
+public:
+    ModelComponent() = delete;
+    ModelComponent(const ModelComponent &) = delete;
+    ModelComponent(shared_ptr<Model> model);
+    ModelComponent &operator=(const ModelComponent &) = default;
+    virtual bool isVisible() const noexcept override;
+    //    virtual void accept(const BaseComponentVisitor &visitor) override;
+
+    shared_ptr<Model> getModel();
+    void setModel(const shared_ptr<Model>);
+};
+
+class CameraComponent : public Component
+{
+private:
+    shared_ptr<CameraComponent> camera;
+
+public:
+    CameraComponent() = delete;
+    CameraComponent(const ModelComponent &) = delete;
+    CameraComponent(shared_ptr<CameraBase> camera);
+    CameraComponent &operator=(const CameraComponent &) = default;
+    virtual bool isVisible() const noexcept override;
+    //    virtual void accept(const BaseComponentVisitor &visitor) override;
+
+    shared_ptr<CameraBase> getCamera();
+    void setModel(const shared_ptr<CameraBase>);
 };
 
 #endif // COMPONENT_H
