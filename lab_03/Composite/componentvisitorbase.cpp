bf2528c2d64a9dc32c0bd96bd0c93fc2a703107e
xAMATEURx <dimakrok@bk.ru>
2020-06-12 23:38:47 +0300

    ADD: constructors for visitors

--- lab_03/Composite/componentvisitorbase.cpp
+++ lab_03/Composite/componentvisitorbase.cpp
@@ -1,6 +1,9 @@
 #include "componentvisitorbase.h"
 
-ComponentVisitorBase::ComponentVisitorBase()
-{
+RotateVisitor::RotateVisitor(double ang, axis axi) : angle(ang), ax(axi) {}
 
-}
+//DrawVisitor::DrawVisitor(shared_ptr<>...)
+
+MoveVisitor::MoveVisitor(double dx, double dy, double dz) : xDelta(dx), yDelta(dy), zDelta(dz) {}
+
+ScaleVisitor::ScaleVisitor(double coef) : coefficient(coef) {}
